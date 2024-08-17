125d7f44371d7210a936a1e0b905652a6f9242a1
osw6858 <osw7875@naver.com>
2024-08-17 17:22:56 +0900

    :tada: 프로젝트 초기화

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,36 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        dark: {
+          background: '#000000',
+          text: '#FFFFFF',
+          gray: {
+            dark: '#2F3336',
+            light: '#71767B',
+          },
+        },
+        light: {
+          background: '#FFFFFF',
+          text: '#0F1419',
+          gray: {
+            dark: '#536471',
+            light: '#EFF3F4',
+          },
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
