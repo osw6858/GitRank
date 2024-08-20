90082ac6e6c645dfc54d5cc5e5561bab92438b82
osw6858 <osw7875@naver.com>
2024-08-20 22:00:02 +0900

    :coffin: middleware.ts 에서 의미없는 콜백함수 옵션 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,23 @@
 import {NextResponse} from 'next/server';
 import {withAuth} from 'next-auth/middleware';
 
-export default withAuth(
-  function middleware(req) {
-    const path = req.nextUrl.pathname;
-    console.log('미들웨어 실행:', path, '토큰:', req.nextauth.token);
+export default withAuth(function middleware(req) {
+  const path = req.nextUrl.pathname;
+  console.log('미들웨어 실행:', path, '토큰:', req.nextauth.token);
 
-    if (req.nextauth.token && path === '/success') {
-      return NextResponse.next();
-    }
-    if (!req.nextauth.token && path === '/') {
-      return NextResponse.next();
-    }
+  if (req.nextauth.token && path === '/success') {
+    return NextResponse.next();
+  }
+  if (!req.nextauth.token && path === '/') {
+    return NextResponse.next();
+  }
 
-    if (req.nextauth.token) {
-      return NextResponse.redirect(new URL('/success', req.url));
-    } else {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-  },
-  {
-    callbacks: {
-      authorized: () => true,
-    },
-  },
-);
+  if (req.nextauth.token) {
+    return NextResponse.redirect(new URL('/success', req.url));
+  } else {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+});
 
 export const config = {
   matcher: ['/success', '/'],
