1463f188c33e99527781193af63ac377c316e956
osw6858 <osw7875@naver.com>
2024-08-22 20:04:32 +0900

    :bug: 토큰이 없는데 인증이 필요한 페이지에 접속시 next-auth 제공 로그인 페이지가 아닌 직접 만든 로그인 페이지로 돌아가게 설정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,9 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
   ],
+  pages: {
+    signIn: '/',
+  },
 };
 
 export const handler = NextAuth(authOptions);
