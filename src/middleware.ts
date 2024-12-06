47810d0fc4b12cd7e90f038bebef1fe6fdf3a8f6
osw6858 <osw7875@naver.com>
2024-12-06 15:04:10 +0900

    :bug: 회원가입 및 로그인 모달이 열리지 않았던 문제 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,18 @@
 import {auth} from '@/auth';
 
+const PROTECTED_URL = ['/success'];
+
+const NOT_PROTECTED_URL = ['/', '/auth/signin', '/auth/signup'];
+
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== '/') {
+  // 로그 아웃 상태에서 protected 라우트에 접근
+  if (!req.auth && !NOT_PROTECTED_URL.includes(req.nextUrl.pathname)) {
     const newUrl = new URL('/', req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 
-  if (req.auth && req.nextUrl.pathname === '/') {
+  // 로그인 상태에서 로그인 페이지 or 회원가입 페이지 접근
+  if (req.auth && !PROTECTED_URL.includes(req.nextUrl.pathname)) {
     const newUrl = new URL('/success', req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
